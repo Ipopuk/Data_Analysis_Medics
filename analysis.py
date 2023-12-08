37c175e2dc9d9edea27e6cfc716ca77374798ec7
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-12-08 04:20:10 +0300

    diabetes mellitus

--- analysis.py
+++ analysis.py
@@ -10,6 +10,18 @@ def analysis(df: pd.DataFrame) -> None:
 
 
 def dependence_time_fact(df: pd.DataFrame) -> None:
+    # Подпункт 1
+    diabetes_mellitus_true = round(((sum(df.loc[:, "сахарный_диабет"]) / sum(df["развитие_опп"])) * 100), 4)
+    diabetes_mellitus_false = round(((sum(df.loc[:, "сахарный_диабет"]) / (len(df) - sum(df["развитие_опп"]))) * 100),
+                                    4)
+    hypertension_true = ""
+    hypertension_false = ""
+    chronic_kidney_disease_true = ""
+    chronic_kidney_disease_false = ""
+
+    print(str(diabetes_mellitus_true) + "%", str(diabetes_mellitus_false) + "%")
+    # print(str(hypertension_true) + "%", str(hypertension_false) + "%")
+    # print(str(chronic_kidney_disease_true + "%"), str(chronic_kidney_disease_false) + "%")
     # Подпункт 3
     print(df[["инфаркт_миокарда", "длительность_операции"]].corr())
 
