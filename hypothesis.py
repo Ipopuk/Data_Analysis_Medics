665b696e7139cbe6afdbeb6f9b2dc2f14039dca1
Ipopuk <honenene@yandex.ru>
2023-12-19 21:39:01 +0300

    hypo norm

--- hypothesis.py
+++ hypothesis.py
@@ -11,17 +11,19 @@ from preprocess import preprocess
 
 def is_normal(df, col_2):
     if shapiro(df[col_2])[1] >= 0.05:
-        print(ttest_ind(df['развитие_опп'], df[col_2]))
+        c_p = ttest_ind(df['развитие_опп'], df[col_2])
 
     else:
-        print(mannwhitneyu(df['развитие_опп'], df[col_2]))
+        c_p = ttest_ind(df['развитие_опп'], df[col_2])
+    print(round(c_p[0], 4), round(c_p[1], 4))
     sns.boxplot(data=df, x='развитие_опп', y=col_2)
     plt.show()
 
 
 def chi_2(df, col_2):
     cross_tab = pd.crosstab(df['развитие_опп'], df[col_2])
-    print(chi2_contingency(cross_tab))
+    c_p = chi2_contingency(cross_tab)
+    print(round(c_p[0], 4), round(c_p[1], 4))
     sns.heatmap(cross_tab, cmap="YlGnBu", annot=True, cbar=False);
     plt.show()
 
@@ -32,6 +34,11 @@ def hypothesis(df: pd.DataFrame) -> None:
     hypo_3(df)
     hypo_4(df)
     hypo_5(df)
+    hypo_6(df)
+    hypo_7(df)
+    hypo_8(df)
+    hypo_9(df)
+    hypo_10(df)
 
 
 def hypo_1(df) -> None:  # 'хбп' хи квадрат
@@ -39,7 +46,7 @@ def hypo_1(df) -> None:  # 'хбп' хи квадрат
 
 
 def hypo_2(df) -> None:  # 'возраст' статы маняуитни
-    isnormal(df, "возраст")
+    is_normal(df, "возраст")
     print(pointbiserialr(df['развитие_опп'], df["возраст"]))
 
 
@@ -48,33 +55,37 @@ def hypo_3(df) -> None:  # 'пол'
 
 
 def hypo_4(df) -> None:  # 'мочевина' маняуитни
-    isnormal(df, 'мочевина')
+    is_normal(df, 'мочевина')
     print(pointbiserialr(df['развитие_опп'], df["мочевина"]))
 
 
 def hypo_5(df) -> None:  # 'лейкоциты_крови' маняуитни
-    isnormal(df, 'лейкоциты_крови')
+    is_normal(df, 'лейкоциты_крови')
     print(pointbiserialr(df['развитие_опп'], df["лейкоциты_крови"]))
 
 
-def hypo_6():  # 'тромбоциты'маняуитни
-    pass
+def hypo_6(df) -> None:  # 'тромбоциты'маняуитни
+    is_normal(df, "тромбоциты")
+    print(pointbiserialr(df['развитие_опп'], df["тромбоциты"]))
 
 
-def hypo_7():  # 'общий_белок'маняуитни
-    pass
+def hypo_7(df) -> None:  # 'общий_белок'маняуитни
+    is_normal(df, "общий_белок")
+    print(pointbiserialr(df['развитие_опп'], df["общий_белок"]))
 
 
-def hypo_8():  # 'аик'  pointbiserialr
-    pass
+def hypo_8(df) -> None:  # 'аик'
+    chi_2(df, 'аик')
 
 
-def hypo_9():  # 'объем_гемотрансфузии'маняуитни
-    pass
+def hypo_9(df) -> None:  # 'объем_гемотрансфузии'маняуитни
+    is_normal(df, "объем_гемотрансфузии")
+    print(pointbiserialr(df['развитие_опп'], df["объем_гемотрансфузии"]))
 
 
-def hypo_10():  # 'диурез'маняуитни
-    pass
+def hypo_10(df):  # 'диурез'маняуитни
+    is_normal(df, "диурез")
+    print(pointbiserialr(df['развитие_опп'], df["диурез"]))
 
 
 if __name__ == "__main__":
