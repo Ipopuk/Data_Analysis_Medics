c4978286a2387a5cddf6db7c4a0d29cdcfca97b0
Ipopuk <honenene@yandex.ru>
2023-12-19 14:39:50 +0300

    hypothesis mini clear

--- hypothesis.py
+++ hypothesis.py
@@ -7,55 +7,73 @@ from preprocess import preprocess
 """
 'развитие_опп'
 """
-def isnormal(col_2):
-    if shapiro(col_2)[1] >= 0.05:
-        #ttest
-        pass
+
+
+def isnormal(df, col_2):
+    if shapiro(df[col_2])[1] >= 0.05:
+        print(ttest_ind(df['развитие_опп'], df[col_2]))
+
     else:
-        mannwhitneyu(col_2)
+        print(mannwhitneyu(df['развитие_опп'], df[col_2]))
+    sns.boxplot(data=df, x='развитие_опп', y=col_2)
+    plt.show()
+
+
+def chi_2(df, col_2):
+    cross_tab = pd.crosstab(df['развитие_опп'], df[col_2])
+    print(chi2_contingency(cross_tab))
+    sns.heatmap(cross_tab, cmap="YlGnBu", annot=True, cbar=False);
+    plt.show()
 
 
 def hypothesis(df: pd.DataFrame) -> None:
-       pass
+    hypo_1(df)
+    hypo_2(df)
+    hypo_3(df)
+    hypo_4(df)
+    hypo_5(df)
 
 
-def hypo_1(): # 'хбп' хи квадрат
+def hypo_1(df) -> None:  # 'хбп' хи квадрат
+    chi_2(df, 'хбп')
+
+
+def hypo_2(df) -> None:  # 'возраст' статы маняуитни
+    isnormal(df, "возраст")
+    print(pointbiserialr(df['развитие_опп'], df["возраст"]))
+
+
+def hypo_3(df) -> None:  # 'пол'
+    chi_2(df, 'пол')
+
+
+def hypo_4(df) -> None:  # 'мочевина' маняуитни
+    isnormal(df, 'мочевина')
+    print(pointbiserialr(df['развитие_опп'], df["мочевина"]))
+
+
+def hypo_5(df) -> None:  # 'лейкоциты_крови' маняуитни
+    isnormal(df, 'лейкоциты_крови')
+    print(pointbiserialr(df['развитие_опп'], df["лейкоциты_крови"]))
+
+
+def hypo_6():  # 'тромбоциты'маняуитни
     pass
 
 
-def hypo_2(): # 'возраст' статы маняуитни
+def hypo_7():  # 'общий_белок'маняуитни
     pass
 
 
-def hypo_3(): # 'пол' pointbiserialr
+def hypo_8():  # 'аик'  pointbiserialr
     pass
 
 
-def hypo_4(): #'мочевина' маняуитни
+def hypo_9():  # 'объем_гемотрансфузии'маняуитни
     pass
 
 
-def hypo_5(): #'лейкоциты_крови' маняуитни
-    pass
-
-
-def hypo_6(): #'тромбоциты'маняуитни
-    pass
-
-
-def hypo_7(): #'общий_белок'маняуитни
-    pass
-
-
-def hypo_8(): #'аик'  pointbiserialr
-    pass
-
-
-def hypo_9(): #'объем_гемотрансфузии'маняуитни
-    pass
-
-
-def hypo_10(): #'диурез'маняуитни
+def hypo_10():  # 'диурез'маняуитни
     pass
 
 
